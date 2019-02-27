c981dfe6e008f175160153a184e3e9772ed2256c
Maurício J Gomes <maur.gomes@gmail.com>
2019-02-27 15:39:53 -0300

    Adiçao de rotas; pequenos ajustes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule }  from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule }        from '@angular/platform-browser';
+import { NgModule }             from '@angular/core';
+import { FormsModule }          from '@angular/forms';
+import { HttpClientModule }     from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TabelasAliquotasComponent } from './tabelas-aliquotas/tabelas-aliquotas.component';
+
+const rotas: Routes = [
+  {path: 'tabela-aliquotas', component: TabelasAliquotasComponent }
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +19,11 @@ import { TabelasAliquotasComponent } from './tabelas-aliquotas/tabelas-aliquotas
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      rotas,
+      {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
