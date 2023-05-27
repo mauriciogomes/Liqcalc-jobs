7e1fc6ef25d31c90b58a6810f163f3a0af44c71f
Mauricio <maur.gomes@gmail.com>
2023-05-27 16:35:16 -0300

    Fixed start-page routes

--- src/app/start-page/start-page.module.ts
+++ src/app/start-page/start-page.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+// Component
 import { StartPageComponent } from './start-page.component';
 
-
+const routes = [{
+  path: '',
+  component: StartPageComponent
+}];
 
 @NgModule({
   declarations: [StartPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild( routes )
   ]
 })
 export class StartPageModule { }
