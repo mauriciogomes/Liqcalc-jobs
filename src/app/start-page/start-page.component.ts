7e739e832b2f2f43bb9d694a49080fa751e4a728
Mauricio <maur.gomes@gmail.com>
2023-06-27 19:16:48 -0300

    feat(StartPage): add hero-section and custom material's input field

--- src/app/start-page/start-page.component.ts
+++ src/app/start-page/start-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-start-page',
   templateUrl: './start-page.component.html',
-  styleUrls: ['./start-page.component.css']
+  styleUrls: ['./start-page.component.css', './custom-material.scss']
 })
 export class StartPageComponent implements OnInit {
 
