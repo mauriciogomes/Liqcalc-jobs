fa886310af89262ed9ad97b226e035e195a17977
Mauricio <maur.gomes@gmail.com>
2023-08-03 18:32:53 -0300

    feat(Search): search page's layout on mobile

---
+++ src/app/opportunities/ui/opportunity-card/opportunity-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Opportunity } from 'src/app/shared/opportunity.model';
+
+@Component({
+  selector: 'app-opportunity-card',
+  templateUrl: './opportunity-card.component.html',
+  styleUrls: ['./opportunity-card.component.css']
+})
+export class OpportunityCardComponent implements OnInit {
+
+  @Input() value: Opportunity;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
